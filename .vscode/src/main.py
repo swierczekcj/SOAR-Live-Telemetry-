6ea41fad2b881d414489f1ecb8b67dfd664630b1
Cole Swierczek <swierczekcj@gmail.com>
2025-02-22 22:28:14 -0500

    we do not care

--- .vscode/src/main.py
+++ .vscode/src/main.py
@@ -4,15 +4,17 @@ import matplotlib.pyplot as plt
 import datetime
 import csv
 
+from functools import partial
 from matplotlib.animation import FuncAnimation
+from matplotlib.widgets import Button
 from scipy.interpolate import make_interp_spline
 
 #globals
 portsList = []
 pressureData = []
 forceData = []
-graphTime = []
-
+timeData = []
+receive = False;
 
 #setting up our serial communications
 ports = serial.tools.list_ports.comports()
@@ -27,7 +29,7 @@ for i in range(len(portsList)):
         want = "COM" + str(com)
         print(want)
 
-dataPort = serial.Serial(want, 115200) #initializing and opening the serial port
+dataPort = serial.Serial(want, 115200, timeout = 3) #initializing and opening the serial port
 
 #setting up our CSV for storage
 f = ["Pressure", "Force", "Time"]
@@ -38,57 +40,96 @@ with open('data.csv', 'w') as file:
 
 #function for parsing data
 def parseGraphData(toParse):
-    info = str(toParse).split(",")
-    pressureVal = info[0].split(":")[1]
-    index = info[1].split(":")[1].find("\\r")
-    forceVal = (info[1].split(":")[1])[:index]
-    return float(pressureVal), float(forceVal)
+    try:
+        i = str(toParse).find("'")
+        info = str(toParse)[i+1:].split(',')
+        time = info[0]
+        force = info[1]
+        i = info[2].find("\\r")
+        pressure= info[2][:i]
+        return float(time), float(force), float(pressure)
+    except Exception as e:
+        return 0, 0, 0
 
 
 
 
+#ping button
+def ping(i):
+    command = "PING"
+    dataPort.write(command.encode('utf-8'))
+    print("pinged")
+    data = dataPort.readline()
+    print(data)
+    
+def launch(i):
+    receive = True;
+    command = "FIRE"
+    dataPort.write(command.encode('utf-8'))
+    
+    
 
 #some final initialization
 fig, (ax1, ax2) = plt.subplots(2,1)
-command = input("Enter command LAUNCH: ")
-dataPort.write(command.encode('utf-8'))
+ax1.plot(timeData, pressureData)
+ax2.plot(timeData, forceData)
+ax1.set_title("Pressure and Force over time")
+ax1.set_ylabel("Pressure (psi)")
+ax2.set_ylabel("Force (lbs)")
+ax2.set_xlabel("Time (μs)")
+# command = input("LAUNCH ")
+# dataPort.write(command.encode('utf-8'))
 startTime = datetime.datetime.now()
 endTime = startTime + datetime.timedelta(seconds=10) #initialize a timer
 plt.style.use('fivethirtyeight')
-
-
+#main animation loop
 def animation(i):
-    
-    #read in our data and throw it into matplotlib
-    if dataPort.in_waiting == 0:
-        pass
-    data = dataPort.readline()     
-    pressure, force = parseGraphData(data)
-    pressureData.append(pressure) #the data we want from mcu
-    forceData.append(force)
-    currTime=(datetime.datetime.now() - startTime).total_seconds() -1 #idk why this -1 is needed but it is
-    graphTime.append(currTime)
-    
-    #store it
-    with open('data.csv', 'a') as file:
-        writer = csv.DictWriter(file, fieldnames=f)
-        info = {
-            f[0]:pressure,
-            f[1]:force,
-            f[2]:currTime
-        }
-        writer.writerow(info)
     
     #graph it
     ax1.cla()
     ax2.cla()
     ax1.set_title("Pressure and Force over time")
-    ax1.set_ylabel("Pressure (kPa)")
-    ax2.set_ylabel("Force (N)")
-    ax2.set_xlabel("Time (s)")
-    ax1.plot(graphTime, pressureData)
-    ax2.plot(graphTime, forceData)
+    ax1.set_ylabel("Pressure (psi)")
+    ax2.set_ylabel("Force (lbs)")
+    ax2.set_xlabel("Time (μs)")
+    ax1.plot(timeData, pressureData)
+    ax2.plot(timeData, forceData)
     plt.tight_layout()
+    
+    # print("here1")
+    #read in our data and throw it into matplotlib
+    # if dataPort.in_waiting == 0:
+    #     pass
+    
+        
+    # print("here2")
+    try:
+        if (dataPort.in_waiting != 0):
+            data = dataPort.readline() 
+            time, force, pressure = parseGraphData(data)
+            pressureData.append(pressure) #the data we want from mcu
+            forceData.append(force)
+            timeData.append(time) 
+            #store it
+            with open('data.csv', 'a') as file:
+                writer = csv.DictWriter(file, fieldnames=f)
+                info = {
+                    f[0]:time,
+                    f[1]:force,
+                    f[2]:pressure
+                }
+                writer.writerow(info) 
+        
+          
+    except serial.SerialException as e:
+        print(e)
+        
+    # print("here3")
+
+    
+    
+    
+    
 
     #check if we have reached the end of the timer
     # currT = datetime.datetime.now()
@@ -98,7 +139,13 @@ def animation(i):
 
 
 
-ani = FuncAnimation(plt.gcf(), animation, interval=100)
-plt.show()
 
+ani = FuncAnimation(plt.gcf(), partial(animation), interval=100)
+axping = fig.add_axes([0.7, 0.01, 0.1, 0.075])
+axfire = fig.add_axes([0.81, 0.01, 0.1, 0.075])
+pingButton = Button(axping, "PING")
+pingButton.on_clicked(ping) 
+launchButton = Button(axfire, "FIRE")
+launchButton.on_clicked(launch)
+plt.show()
 
